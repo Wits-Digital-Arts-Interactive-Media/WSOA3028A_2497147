b3b85108107ddac1b2f54fbad49fc1e26b1e5735
mthokub23 <2497147@students.wits.ac.za>
2024-04-29 10:43:53 +0200

    Content Update

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ document.addEventListener('DOMContentLoaded', function() {
 function goToPage(page) {
     window.location.href = page;
 }
+
+//Function to track slide movement on portfolio
+const track = document.getElementById("image-track");
+
+
+window.onmousedown = e => {
+    track.dataset.e.clientX;
+}
+
+window.onmousemove = e =>{
+
+if(track.dataset.mouseDownAt ==="0")return;
+
+const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+      maxDelta = window.innderWidth/ 2;
+
+const percentage = (mouseDelta /maxDelta)* 100;
+        nextPercentage = parseFloat(track.dataset.prevPercentage)+ percentage;
+track.dataset.prevPercentage = nextPercentage;
+
+track.style.transform = `translate(${percentage}%, -50)`;
+}
+
+window.onmouseup = () =>{
+
+    track.daataset.mouseDownAt ="0"; 
+    
+}
+
+//Slider incomplete because styling is incomplete
